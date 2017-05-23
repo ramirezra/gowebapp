c272a8d129798f90bf9878ff2fe8309d7e9d5009
Robinson Ramirez <robinson@camrpc.com>
2017-05-23 18:10:58 -0400

    Chapter 4.

---
+++ 04/4.15/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func setCookie(w http.ResponseWriter, r *http.Request) {
+	c1 := http.Cookie{
+		Name:     "first_cookie",
+		Value:    "Go Web Programming",
+		HttpOnly: true,
+	}
+	c2 := http.Cookie{
+		Name:     "second_cookie",
+		Value:    "Manning Publications Co",
+		HttpOnly: true,
+	}
+	// w.Header().Set("Set-Cookie", c1.String())
+	http.SetCookie(w, &c1)
+	http.SetCookie(w, &c2)
+}
+
+func getCookie(w http.ResponseWriter, r *http.Request) {
+	h := r.Header["Cookie"]
+	fmt.Fprint(w, h)
+}
+
+func main() {
+	server := http.Server{
+		Addr: "127.0.0.1:8080",
+	}
+	http.HandleFunc("/set_cookie", setCookie)
+	http.HandleFunc("/get_cookie", getCookie)
+	server.ListenAndServe()
+}
